1f59fd1657af248f6486917758d965a83367853d
Ondřej Henzl <greenclon@seznam.cz>
2020-11-05 23:19:40 +0100

    Going for profiler

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController {
+class HomeController extends AbstractController {
   public function homepage() {
 
     return new Response('ASDFG');
+  }
+
+  public function show() {
+
+    return $this->render('home/show.html.twig', [
+      'question' => 'hey how are you'
+    ]);
+
   }
 }
 
