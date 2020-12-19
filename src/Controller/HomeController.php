8d41acc4717003cb4a912db1a87fd9564236eb65
Ondřej Henzl <greenclon@seznam.cz>
2020-12-19 17:16:08 +0100

    Trying to add login throttling, before symfony upgrade

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -47,28 +47,7 @@ class HomeController extends AbstractController {
 
   }
 
-  public function logged() {
 
-    if ($this->session->get('logged') === false) {
-      require_once('../src/scripts/login.php');
-      $errinfo = checkLoginInformation($_POST["name"], $_POST["password"], $this->session);
-    }
-
-
-    if ($this->session->get('logged') === true) {
-      return $this->render('home/logged.html.twig', [
-      ]);
-    } else {
-      return $this->redirectToRoute('login', ['failed' => $errinfo]);
-    }
-  }
-
-  public function logout() {
-    $this->session->set('logged', false);
-    $this->session->set('level', '');
-    $this->session->set('name', '');
-    return $this->redirectToRoute('login');
-  }
 }
 
 
