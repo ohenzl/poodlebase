3881695a39f6a01e6511f1a0e7e893d3012fc58a
Ondřej Henzl <greenclon@seznam.cz>
2021-01-08 18:09:12 +0100

    Odebrani nepouzivanych funkci pouzivanych pro verzi s ID rodici ve vrhu

--- src/scripts/form.php
+++ src/scripts/form.php
@@ -9,44 +9,6 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class FormToSQL {
 
-  function pridatPsa($jmeno, $chovatelska_stanice, $conn, $user, $pohlavi) {
-    //vytváření vrh pro rodiče pouze s chovatelskou stanicí a získání ID
-    $datetime = date("Y-m-d H:i:s");
-    //sql pro vložení rodiče
-    $co = "VALUES ('$chovatelska_stanice', '$user', '$datetime');";
-    $kam = "INSERT INTO vrh (stanice , vloz_osoba, vloz_datum) ";
-    $sql = $kam . $co;
-
-    if ($conn->query($sql) === TRUE) {
-      $vrh_id = $conn->insert_id;  //získání ID
-    } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-    //zapsání rodiče
-    $co = "VALUES ('$jmeno', '$pohlavi', '$vrh_id', '$user', '$datetime');";
-    $kam = "INSERT INTO psi (jmeno, pohlavi, vrh, vloz_osoba, vloz_datum) ";
-    $sql = $kam . $co;
-    //zápis psa, získání ID rodiče
-    if ($conn->query($sql) === TRUE) {
-      $pes_id = $conn->insert_id;
-      return $pes_id;  //získání ID
-    } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-  }
-
-  function kontrolaDatabaze($jmeno, $chovatelska_stanice, $conn, $pohlavi, $user) {
-    $sql = "SELECT p.ID ID FROM psi p JOIN vrh v ON p.vrh=v.ID WHERE p.jmeno = '$jmeno' and v.stanice = '$chovatelska_stanice'";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-        return $row['ID'];
-      }
-    } else {
-      return $this->pridatPsa($jmeno, $chovatelska_stanice, $conn, $user, $pohlavi);
-    }
-  }
-
 
   function parsePostVrh($input, $conn, $user) {
     $vrh = new Vrh;
@@ -57,14 +19,6 @@ class FormToSQL {
         $vrh->$name = $value;
       }
     }
-    //kontrola otce
-    // if ($input['otec_jmeno1'] !== '') {
-    //   $vrh->otec_ID = $this->kontrolaDatabaze($input['otec_jmeno1'], $input['otec_chov1'], $conn, 'pes', $user);
-    // }
-    // //kontrola matky
-    // if ($input['matka_jmeno1'] !== '') {
-    //   $vrh->matka_ID = $this->kontrolaDatabaze($input['matka_jmeno1'], $input['matka_chov1'], $conn, 'fena', $user);
-    // }
     return $vrh;
   }
 
